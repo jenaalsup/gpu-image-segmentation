85ecc34a06da75ca845a2ddbfff24ee0c995f5d7
jenaalsup <jena.alsup@gmail.com>
2025-06-05 02:57:41 -0700

    remove unnecessary thing

--- src/gpu/threshold.cuh
+++ src/gpu/threshold.cuh
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <opencv2/core.hpp>
-
 int findOtsuThreshold(const unsigned char* d_input, int width, int height);
 
 void runThresholding(const unsigned char* h_input, unsigned char* h_output, int width, int height);
